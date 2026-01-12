7948cff1b9c607fb92245affd7b6ae9e357319ae
Sophoun <sophoun@debian.local>
2026-01-12 13:28:35 +0700

    add to image

--- lib/src/extension/string_extension.dart
+++ lib/src/extension/string_extension.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 extension StringExtension on String? {
   /// Return null or value but not empty
   String? get orNull {
@@ -22,4 +25,43 @@ extension StringExtension on String? {
 
   /// Return default string or value but not null
   String orDefault(String value) => this ?? value;
+}
+
+/// SVG File path to Image object
+extension StringExtendToSvg on String? {
+  Widget toImage({
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Alignment alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    Widget Function(BuildContext)? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+    Widget Function(BuildContext, Object, StackTrace)? errorBuilder,
+    SvgTheme? theme,
+    ColorMapper? colorMapper,
+  }) {
+    return SvgPicture.asset(
+      this!,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      clipBehavior: clipBehavior,
+      errorBuilder: errorBuilder,
+      theme: theme,
+      colorMapper: colorMapper,
+    );
+  }
 }
